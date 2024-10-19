1ab25f3dfbe8f71ab1612a67d2b4109cb1d885b9
Atul-raj07 <atulrajgupta12j@gmail.com>
2024-10-19 19:25:09 +0530

    creating a base url

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
-import Nav from './components/Nav'
-import Api from "../utils/Api"
+import React from "react";
+import Nav from "./components/Nav";
+import axios from "./utils/Api";
 
 const App = () => {
+  const products = async () => {
+    const api = "/products";
+
+    const products = await axios.get(api);
+    console.log(products.data);
+  };
 
   return (
     <>
       App
       <Nav />
-      <Api />
-      </>
-  )
-}
+      
+      <div>
+        <button onClick={products}>get products</button>
+      </div>
+    </>
+  );
+};
 
-export default App
+export default App;
